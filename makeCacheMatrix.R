341f32ddf7adaf562f0f24429899b4e7b1676f15
terencekudo <terencekudo@gmail.com>
2014-07-26 22:06:11 -0400

    Programming 2 Submission
    Programming 2 Submission for Terence Kudo

---
+++ makeCacheMatrix.R
@@ -0,0 +1,14 @@
+makeCacheMatrix <- function(x=numeric()) {
+	m <- NULL
+	set <- function(y) {
+		x<<-y
+		m<<-NULL
+	}
+	
+	get <- function() x
+	setM <- function(solve) m <<- solve
+	getM <- function()m
+	list(set = set, get = get, 
+		setM = setM, 
+		getM = getM)
+}
